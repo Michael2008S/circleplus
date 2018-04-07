6aaf6849bbaf6c3af7fcf0cf6ba469df63e6888e
Michael.Shieh <michael2008s@gmail.com>
2018-04-08 01:22:45 +0800

    config truffle

--- truffle.js
+++ truffle.js
@@ -1,3 +1,22 @@
+// For working with INFURA
+const HDWalletProvider = require("truffle-hdwallet-provider")
+const fs = require("fs")
+
+// First read in the secrets.json to get our mnemonic
+let secrets
+let mnemonic
+if (fs.existsSync("secrets.json")) {
+    secrets = JSON.parse(fs.readFileSync("secrets.json", "utf8"))
+    mnemonic = secrets.mnemonic
+} else {
+    console.log("No secrets.json found. If you are trying to publish EPM " +
+        "this will fail. Otherwise, you can ignore this message!")
+    mnemonic = "thing burst differ file brown alert lunch giant book web emerge awkward"
+}
+
+var infura_apikey = "VJGiIBhLMhHDsBQ09MJu";
+
+
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // to customize your Truffle configuration!
@@ -8,7 +27,29 @@ module.exports = {
             port: 7545,
             network_id: "*"
         },
+
+        ropsten: {
+            host: "127.0.0.1",
+            port: 8547,
+            network_id: "3" // Match any network id
+        },
+
+
+        ropsten_inf: {
+            // provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/VJGiIBhLMhHDsBQ09MJu"),
+            provider: function() {
+                return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey)
+            },        port: 8545,
+            network_id: 3,
+            //  from: "0x0073a284FE9C6f9ad578F23E2397BF2fe6De59A1"
+            gasPrice: 2000000000,
+            gas: 6000000
+
+        },
+
     },
+
+
 
 
     // solc: {

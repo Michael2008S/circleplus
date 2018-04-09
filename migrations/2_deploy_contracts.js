2a3b24fc1baec3a79d8b349da166f00489aef2c3
Michael.Shieh <michael2008s@gmail.com>
2018-04-09 08:43:03 +0800

    add crowdsale code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,22 @@
 var Circle = artifacts.require("./Circle.sol");
+var CircleCrowdsale = artifacts.require("./CircleCrowdsale.sol");
 
-module.exports = function (deployer) {
+module.exports = function(deployer) {
     // Deploying..
-    deployer.deploy(Circle);
+
+  // deployer.deploy(SafeMath);
+  // deployer.link(SafeMath, Circle);
+  // deployer.deploy(Addresses);
+  // deployer.link(Addresses, Circle);
+
+    deployer.deploy(Circle).then(function() {
+        return deployer.deploy(
+            CircleCrowdsale,
+            Circle.address,
+            web3.eth.blockNumber,
+            web3.eth.blockNumber + 1000,
+            web3.toWei(1, 'ether'),
+            1
+        ).then(function() {});
+    });
 };

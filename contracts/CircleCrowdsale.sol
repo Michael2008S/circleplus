b72051f54a145f6da89a7e8490af1778ff148a50
Michael.Shieh <michael2008s@gmail.com>
2018-04-09 15:36:42 +0800

    实现该分支代码

--- contracts/CircleCrowdsale.sol
+++ contracts/CircleCrowdsale.sol
@@ -1,85 +1,102 @@
 pragma solidity ^0.4.18;
 
-import "./ERC223ReceivingContract.sol";
-import "./SafeMath.sol";
-import "./ERC20.sol";
-import "./Token.sol";
+import "./Circle.sol";
+import "./zeppelin/crowdsale/validation/CappedCrowdsale.sol";
 
-contract CircleCrowdsale is ERC223ReceivingContract {
+contract CircleCrowdsale is CappedCrowdsale {
 
-    using SafeMath for uint;
+    // Crowdsale Stage
+    // ============
+    enum CrowdsaleStage {
+        AngelRound,
+        PreSaleRound,
+        OpenRound}
 
-    Token private _token;
+    // Token Distribution
+    // =============================
+    uint256 public totalSupplyMax = 20000000000 * (10 ** 18); // There will be total 20,000,000,000 Circle Tokens
 
-    uint private _start;
-    uint private _end;
+    uint256 public angelRound = 100000000 * (10 ** 18);   // Angel Investors 100,000,000 (10%)
+    uint256 public preSaleRound = 400000000 * (10 ** 18);   // PreSale Round 400,000,000 (10%)
+    uint256 public openRound = 100000000 * (10 ** 18);   // Open Round 100,000,000 (10%)
 
-    uint private _price;
-    uint private _limit;
-    uint private _available;
+    uint256 public teamFund = 400000000 * (10 ** 18);   // Team/Foundation 400,000,000 (20%) cliff 6mon
+    uint256 public communityFund = 400000000 * (10 ** 18);   // Community 400,000,000 (20%)
+    uint256 public marketingFund = 400000000 * (10 ** 18);   // Marketing 400,000,000 (20%)
+    // ==============================
 
-    mapping(address => uint) private _limits;
+    // Stage Rate
+    // ============
+    uint256 private _angelRate = 60000;
+    uint256 private _preSaleRate = 30000;
+    uint256 private _openRate = 20000;
+    // ============
 
-    event Buy(address beneficiary, uint amount);
+    // angel locked tokens
+    TokenTimelock public angelTimeLock;
 
-    modifier available() {
-        require(_available > 0);
+    // team vesting
+    uint256 public constant TEAM_VESTING_CLIFF = 30*6 days;
+    uint256 public constant TEAM_VESTING_DURATION = 2 years;
 
-        require(block.number >= _start && block.number < _end);
-
-        _;
+    // Constructor
+    // ============
+    function CircleCrowdsale(uint256 _rate, address _wallet, uint256 _goal, uint256 _cap)
+    CappedCrowdsale(_cap)
+    FinalizableCrowdsale()
+    Crowdsale(_rate, _wallet)
+    public {
+        require(_goal <= _cap);
     }
+    // =============
 
-    modifier isToken() {
-        require(msg.sender == address(_token));
-        _;
+
+    // Token Deployment
+    // =================
+    function createTokenContract() internal returns (MintableToken) {
+        return new Circle();
+        // Deploys the ERC20 token. Automatically called when crowdsale contract is deployed
     }
+    // ==================
 
-    modifier valid(address to, uint amount) {
-        assert(amount > 0);
-        amount = amount.div(_price);
 
-        assert(_limit >= amount);
-
-        assert(_limit >= _limits[to].add(amount));
-
-        _;
-    }
-
-    function CircleCrowdsale(address token, uint start, uint end, uint price, uint limit) public {
-        _token = Token(token);
-        _start = start;
-        _end = end;
-        _price = price;
-        _limit = limit;
-    }
-
-    function() public {
+    function() external payable {
         revert();
     }
 
-    function buy() public payable {
-        return buyFor(msg.sender);
+    function investByLegalTender(address _beneficiary, uint256 _value, uint _stage) external {
+
+        if (stage == CrowdsaleStage.AngelRound) {
+            // give tokens to angel with lock 90 days
+            angelTimeLock = new TokenTimelock(token, _beneficiary, uint64(now + 90 days));
+            token.mint(angelTimeLock, _angelRate * _value);
+        } else if (stage == CrowdsaleStage.PreSaleRound) {
+            token.mint(_beneficiary, _preSaleRate * _value);
+        } else if (stage == CrowdsaleStage.OpenRound) {
+            token.mint(_beneficiary, _openRate * _value);
+        }
     }
 
-    function buyFor(address beneficiary) public available
-    valid(beneficiary, msg.value)
-    payable {
-        uint amount = msg.value.div(_price);
-        _token.transfer(beneficiary, amount);
-        _available = _available.sub(amount);
-        _limits[beneficiary] = _limits[beneficiary].add(amount);
-        Buy(beneficiary, amount);
+    // Finish: Mint Extra Tokens as needed before finalizing the Crowdsale.
+    // ====================================================================
+    function setReservedHolder(address _teamFundWallet, address _communityFundWallet, address _marketingFundWallet){
+        uint256 alreadyMinted = token.totalSupply();
+        require(alreadyMinted < maxTokens);
+
+        TokenVesting _teamTokenVesting = new TokenVesting(_teamFundWallet, now, TEAM_VESTING_CLIFF, TEAM_VESTING_DURATION, true);
+        token.mint(_teamFundWallet, teamFund);
+        token.mint(_communityFundWallet, communityFund);
+        token.mint(_marketingFundWallet, marketingFund);
+
+        // TODO remind token to another wallet
 
     }
+    // ===============================
 
-    function tokenFallback(address, uint _value, bytes) isToken public {
-        _available = _available.add(_value);
+
+    // REMOVE THIS FUNCTION ONCE YOU ARE READY FOR PRODUCTION
+    // USEFUL FOR TESTING `finish()` FUNCTION
+    function hasEnded() public view returns (bool) {
+        return true;
     }
-
-    function availableBalance() view public returns (uint) {
-        return _available;
-    }
-
-
 }
